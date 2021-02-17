b061079819c7b1d81e13de110d05ed8a96d51d5b
ulises-aguilar <54950806+ulises-aguilar@users.noreply.github.com>
2021-02-17 11:03:59 -0800

    Add files via upload

---
+++ helloworld.c
@@ -0,0 +1,9 @@
+// helloworld.c compile : gcc helloworld.c -o helloworld.o
+#include <stdio.h>
+// main() function
+int main() { // main() function as integer
+	// printf() is the same as print() in Python
+	printf("\nHello World!");
+	printf("***********\n");
+	return 0;
+}
